7627543c3f4dbfff16fc4bdeb2f8f8af198ef0a8
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-07-20 23:54:27 +0300

    reconstruntion

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = var.AWS_Region
+}
+
+resource "aws_instance" "myinstance" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  #key_name = var.web_key_name
+  tags = {
+        Name = "vioinstance"
+        owner = "violetta"        
+    }
+}
---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "AWS_Region" {
+    default = "eu-central-1"
+}
+
+variable "ami_id" {
+  default = "ami-0ceb85bb30095410b"
+}
+
+variable "instance_type" {
+  default = "t4g.micro"
+}
+
+#variable "web_key_name" {
+#    default = "key123"
+#}
