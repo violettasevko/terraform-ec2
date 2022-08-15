ac3b37d1c0e1d3020108a1a504e26717b1aaa522
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-16 02:35:43 +0300

    changes

---
+++ ec2/e-module.tf
@@ -0,0 +1,26 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "instance"
+
+  ami                    = "ami-07f16fb14274bfc76"
+  instance_type          = "t4g.small"
+  key_name               = "hoi22"
+  vpc_security_group_ids = ["sg-0bc2447cb2a6e9bc5"]
+  subnet_id              = "subnet-07e2128167d8cbd9d"
+  associate_public_ip_address = true
+
+root_block_device = [
+    {
+      encrypted   = true
+      volume_type = "gp3"
+      volume_size = 8
+    },
+  ]
+
+
+  tags = {
+    owner   = "violetta"
+  }
+}
