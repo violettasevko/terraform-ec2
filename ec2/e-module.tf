ca5887e688bbe557322bf064f871a3916b7a1dae
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-08-23 00:34:56 +0300

    back

--- ec2/e-module.tf
+++ ec2/e-module.tf
@@ -18,17 +18,6 @@ module "ec2_instance" {
     }]
 }
 
-provisoner "file" {
-    sourse = "./script"
-    destination = "./script"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x ./script",
-      "sudo ./script",
-    ]
-  }
-
 resource "aws_volume_attachment" "this" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.this.id
