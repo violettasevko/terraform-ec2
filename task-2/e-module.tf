3231b76f0ae80374872999b856dbb060ebe5f0ca
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2023-03-23 17:05:56 +0300

    cteate a new task

---
+++ task-2/e-module.tf
@@ -0,0 +1,31 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "task-2-vio"
+
+  #ami arm amilinux
+  ami                    = "ami-05fab674de2157a80"
+  instance_type          = "t4g.micro"
+  availability_zone      = "us-east-1a"
+  key_name               = "l1q_key"
+  vpc_security_group_ids = ["sg-008d650fa174f6032"]
+  subnet_id              = "subnet-093c431d9db5d8562"
+  associate_public_ip_address = true
+  user_data = file("script")
+
+  root_block_device = [{ 
+    volume_size = 10
+    volume_type = "gp3"
+    }]
+}
+
+resource "aws_volume_attachment" "this" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.this.id
+  instance_id = module.ec2_instance.id
+}
+resource "aws_ebs_volume" "this" {
+  availability_zone =  "us-east-1a"
+  size = 5
+}
---
+++ task-2/vars.tf
@@ -0,0 +1,15 @@
+variable "AWS_Region" {
+    default = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-05fab674de2157a80"
+}
+
+variable "instance_type" {
+  default = "t4g.micro"
+}
+
+#variable "web_key_name" {
+#    default = "l1q_key"
+#}
